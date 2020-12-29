7a0d4853326a93a75292de6aa2e9b7ca721efd94
VincentBlanco <75444092+VincentBlanco@users.noreply.github.com>
2020-12-29 17:20:38 +0100

    page creation  maison v2

---
+++ pages/page_creation_maison.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>page création maison</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div align="center">
+			<h1>Création maison</h1>
+		</br></br>
+		<from method="POST" action="">
+			<label for="formulaire">
+			Nom:</label>
+			<input type="text" placeholder="Le Nom de la maison" id="nom" name="nom" />
+		</br></br>
+			<label for="formulaire">
+			Rue:</label>
+			<input type="text" placeholder="Adresse de la maison" id="rue" name="rue" />
+		</br></br>
+			<label for="formulaire">
+			Ville:</label>
+			<input type="text" placeholder="Ville" id="ville" name="ville" />
+		</br></br>
+			<label for="formulaire">
+			Code Postale:</label>
+			<input type="text" placeholder="Code Postale" id="CP" name="CP" />
+		</br></br>
+			<label for="formulaire">
+			Département:</label>
+			<input type="text" placeholder="Département" id="dept" name="dept" />
+		</br></br>
+			<label for="formulaire">
+			Région:</label>
+			<input type="text" placeholder="Région" id="region" name="region" />
+		</br></br>
+
+
+
+</body>
+</html>
