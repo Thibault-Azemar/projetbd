d29e7793717573b0a4557328fe6c8f95b7b4a961
VincentBlanco <75444092+VincentBlanco@users.noreply.github.com>
2020-12-29 19:30:20 +0100

    creation maison v3

--- pages/page_creation_maison.php
+++ pages/page_creation_maison.php
@@ -10,27 +10,27 @@
 			<h1>Création maison</h1>
 		</br></br>
 		<from method="POST" action="">
-			<label for="formulaire">
+			<label for="Nom">
 			Nom:</label>
 			<input type="text" placeholder="Le Nom de la maison" id="nom" name="nom" />
 		</br></br>
-			<label for="formulaire">
+			<label for="Rue">
 			Rue:</label>
 			<input type="text" placeholder="Adresse de la maison" id="rue" name="rue" />
 		</br></br>
-			<label for="formulaire">
+			<label for="Ville">
 			Ville:</label>
 			<input type="text" placeholder="Ville" id="ville" name="ville" />
 		</br></br>
-			<label for="formulaire">
+			<label for="Code Postale">
 			Code Postale:</label>
-			<input type="text" placeholder="Code Postale" id="CP" name="CP" />
+			<input type="number" placeholder="Code Postale" id="cp" name="cp" />
 		</br></br>
-			<label for="formulaire">
+			<label for="Département">
 			Département:</label>
 			<input type="text" placeholder="Département" id="dept" name="dept" />
 		</br></br>
-			<label for="formulaire">
+			<label for="Région">
 			Région:</label>
 			<input type="text" placeholder="Région" id="region" name="region" />
 		</br></br>
