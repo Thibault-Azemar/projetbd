18eb9e5ce07f8b9915edb4d94f44951abf95c0e6
LucChereau <luc.chereau@etu.univ-tours.fr>
2020-12-29 18:42:57 +0100

    git commit

--- main.php
+++ main.php
@@ -8,21 +8,29 @@
 <body>
 	<h1>Page D'Accueil </h1>
 	<div align="right" class="Page_anonyme">
-		 <a href="pages/page_anonyme.php"><font color="#0000FF">Page anonyme</a>
+		 <a href="pages/page_anonyme.php"><font color="#0000FF">Page anonyme | </a>
+		 	<a href="pages/Page_Administrateur.php"><font color="#0000FF">Administrateur</a>
 	</div>
 	<font color="#000000">
 	<div class="Connexion">
 		<h2>Connexion</h2>
-		<p>Identifiant : <input type="text" id="name" name="Identifiant"></p>
-		<p>Mot de passe : <input type="password" id="password" name="MotDePasse"></p>
-		<input class="Button"
-       type="button"
-       value="Connexion">
-
+		<form method="POST" action="pages/Page_utilisateur.php">
+			<label for="formulaire_connexion">
+			Identifiant:</label>
+			<input type="text"  id="Identifiant" name="Identifiant" />
+		</br></br>
+			<label for="formulaire_connexion">
+			Mot de passe:</label>
+			<input type="password" id="MDP" name="MotDePasse" />
+		</br></br>
+		<input type="submit" id="boutton" name="boutton" value="Connexion"/>
 	</div>
 	<div class="Création de Compte">
 		<a href="pages/creationcompte.php"><font color="#0000FF">Vous ne possèdez aucun compte ?</a>
 	</div>
-
+<?php 
+		$BDD =new PDO("mysql:host=127.0.0.1;dbname=bddphp","root",""); 
+		
+ ?>
 </body>
 </html>
