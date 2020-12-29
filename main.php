d479de3e324cfc75d019293681f288dbb8b8b5c5
LucChereau <luc.chereau@etu.univ-tours.fr>
2020-12-29 16:21:32 +0100

    Update main.php

--- main.php
+++ main.php
@@ -2,15 +2,25 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Salut</title>
+  <title>Projet BDD </title>
   <link rel="stylesheet" href="style.css">
+  <div class="Titre">
+		<h1>Page D'Accueil </h1>
+	</div>
 </head>
+
 <body>
- <?php 
-		$BDD =new PDO("mysql:host=127.0.0.1;dbname=bddphp","root",""); 
-		$requette=$BDD->query("SELECT * FROM personne "); 
-		$resultat =$requette->fetch(); 
-		echo $resultat['Id_Personne']; 
- ?>
+	//Accueil=page de connexion
+	//Lien page de création de compte 
+	//Lien page sans connexion graphs
+	//Fin de page 
+	<div class="Connexion">
+		<h2>Connexion</h2>
+		<p>Identifiant : <input type="text" id="name" name="Identifiant"></p>
+		<p>Mot de passe : <input type="password" id="password" name="MotDePasse"></p>
+		<input class="Button"
+       type="button"
+       value="Connexion">
+	</div>
 </body>
 </html>
