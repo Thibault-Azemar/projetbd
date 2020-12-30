e9258af918c5dd66cde24a2fafcd966fc1c8879d
Thibault-Azemar <75328677+Thibault-Azemar@users.noreply.github.com>
2020-12-30 18:05:25 +0100

    creationcompte2

--- pages/creationcompte.php
+++ pages/creationcompte.php
@@ -1,3 +1,19 @@
+<?php
+
+$BDD =new PDO("mysql:host=127.0.0.1;dbname=bddphp","root","");
+if(isset($_POST['forminsc']))
+{
+	echo "ok";
+	if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['date_de_naissance']) AND !empty($_POST['email']) AND !empty($_POST['numtel']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2']) )
+	{
+		echo"oui";
+	}
+	else
+	{
+		echo "non";
+	}
+}
+?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -11,36 +27,86 @@
 		</br></br>
 		<from method="POST" action="">
 			<label for="formulaire">
-			Nom:</label>
-			<input type="text" placeholder="Votre Nom" id="nom"name="nom" />
-		</br></br>
-			<label for="formulaire">
-			Prenom:</label>
-			<input type="text" placeholder="Votre Prenom" id="prenom"name="prenom" />
-		</br></br>
-		<label for="formulaire">
-			Genre:</label>
-			<div class="reponses">
-				<input type="radio" name="quest1" value="homme"> Homme<br/>
-				<input type="radio" name="quest1" value="femme"> Femme<br/>
-				<input type="radio" name="quest1" value="non_precise"> Non précisé<br/>
-				<input type="radio" name="quest1" value="autre"> Autre<br/>
-			</div>
-		</br></br>
-			<label for="formulaire">
-			Date de naissance:</label>
-			<input type="date" id="datenaissance"name="date_de_naissance" />
-		</br></br>
-			<label for="formulaire">
-			Email:</label>
-			<input type="email" placeholder="....@...." id="email"name="email" />
-		</br></br>
-			<label for="formulaire">
-			Numéro de telephone:</label>
-			<input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$">
-
-		</br></br>
-
-
+				<table align="center">
+					<tr >
+						<td align="right">
+							<label for="nom">Nom :</label>
+						</td>
+						<td>
+							<input type="text" placeholder="Votre Nom" id="nom"name="nom" />
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="formulaire">Prenom :</label>
+						</td>
+						<td>
+							<input type="text" placeholder="Votre Prenom" id="prenom"name="prenom" />
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="formulaire">Genre :</label>
+						</td>
+						<td>
+							<div class="reponses">
+								<input type="radio" name="quest1" value="homme"> Homme<br/>
+								<input type="radio" name="quest1" value="femme"> Femme<br/>
+								<input type="radio" name="quest1" value="non_precise"> Non précisé<br/>
+								<input type="radio" name="quest1" value="autre"> Autre<br/>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="formulaire">Date de naissance :</label>
+						</td>
+						<td>
+							<input type="date" id="datenaissance"name="date_de_naissance" />
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="formulaire">Email :</label>
+						</td>
+						<td>
+							<input type="email" placeholder="....@...." id="email"name="email" />
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="formulaire">Numéro de telephone :</label>
+						</td>
+						<td>
+							<input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" name="numtel">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="formulaire">Mot de Passe :</label>
+						</td>
+						<td>
+							<input type="password" id="mdp"name="mdp" />
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="formulaire">Confirmer le mot de passe :</label>
+						</td>
+						<td>
+							<input type="password" id="mdp2"name="mdp2" />
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>
+							</br>
+							<input type="submit" name="forminsc" value="Confirmer" />
+						</td>
+					</tr>
+				</table>
+			</label>
+		</from>
+	</div>
 </body>
 </html>
