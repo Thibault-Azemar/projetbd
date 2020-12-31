0abf434f51b9cae0c5874d23904ed6cd6446ae23
Thibault-Azemar <75328677+Thibault-Azemar@users.noreply.github.com>
2020-12-31 14:36:51 +0100

    creationcompte presque fini
    manque plus que l'insertion dans la bdd et quelques verifs

--- pages/creationcompte.php
+++ pages/creationcompte.php
@@ -1,22 +1,41 @@
 <?php 
 
-		$BDD =new PDO("mysql:host=127.0.0.1;dbname=bddphp","root",""); 
-		if(isset($_POST['Boutton_Connex']))
+		$bdd =new PDO("mysql:host=127.0.0.1;dbname=bddphp","root",""); 
+		if(isset($_POST['boutton_creer']))
 		{
-			echo "ok1";
-			if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['date_de_naissance']) AND !empty($_POST['email']) AND !empty($_POST['numtel']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2']) /** AND ($genre != NULL) **/)
+			if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['datenaissance']) AND !empty($_POST['email']) AND !empty($_POST['numtel']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])AND !empty($_POST['genre']))
 			{
-				echo "good";
+				$nom = htmlspecialchars($_POST['nom']);
+				$prenom = htmlspecialchars($_POST['prenom']);
+				$email = htmlspecialchars($_POST['email']);
+				$mdp = sha1($_POST['mdp']);
+				$mdp2 = sha1($_POST['mdp2']);
+
+				
+				if(filter_var($email, FILTER_VALIDATE_EMAIL))
+				{
+					if ($mdp == $mdp2)
+					{
+						echo "c'est ok";
+						$insertintocompte = $bdd->prepare("INSERT INTO personne(email, date_naissance, nom, genre, num_tel, prenom) VALUES(?, ?, ?, ?, ?, ?)");
+						$insertintocompte->execute(array($email, $datenaissance, $nom, $genre, $numtel, $prenom));
+						$erreur = "Votre compte a bien été créé !";
+					}
+					else
+					{
+						$erreur = "Les mots de passes ne correspondent pas !";
+					}
+				}
+				else
+				{
+					$erreur = "L'email n'est pas valide !";
+				}
 			}
 			else
 			{
-				$erreur = "Veuillez remplir tous les champs";
+				$erreur = "Veuillez remplir tous les champs !";
 			}
 
-		}
-		else
-		{
-			echo "pas ok";
 		}
  ?>
  <!doctype html>
@@ -39,7 +58,7 @@
 								<label for="nom">Nom :</label>
 							</td>
 							<td>
-								<input type="text" placeholder="Votre Nom" id="nom"name="nom" />
+								<input type="text" placeholder="Votre Nom" id="nom"name="nom" value="<?php if(isset($nom)) { echo $nom; }?>" />
 							</td>
 						</tr>
 						<tr>
@@ -47,7 +66,7 @@
 								<label for="formulaire">Prenom :</label>
 							</td>
 							<td>
-								<input type="text" placeholder="Votre Prenom" id="prenom"name="prenom" />
+								<input type="text" placeholder="Votre Prenom" id="prenom"name="prenom" value="<?php if(isset($prenom)) { echo $prenom; }?>"  />
 							</td>
 						</tr>
 						<tr>
@@ -55,12 +74,12 @@
 								<label for="formulaire">Genre :</label>
 							</td>
 							<td>
-								<div class="reponses">
-									<input type="radio" name="homme" value="homme"> Homme<br/>
-									<input type="radio" name="femme" value="femme"> Femme<br/>
-									<input type="radio" name="non_precise" value="non_precise"> Non précisé<br/>
-									<input type="radio" name="autre" value="autre"> Autre<br/>
-								</div>
+								<select name="genre">
+									<option>Homme </option>
+									<option>Femme</option>
+									<option>Autre</option>
+									<option selected="yes">Non précisé</option>
+								</select>
 							</td>
 						</tr>
 						<tr>
@@ -68,7 +87,7 @@
 								<label for="formulaire">Date de naissance :</label>
 							</td>
 							<td>
-								<input type="date" id="datenaissance"name="date_de_naissance" />
+								<input type="date" id="datenaissance"name="datenaissance" value="<?php if(isset($datenaissance)) { echo $datenaissance; }?>" />
 							</td>
 						</tr>
 						<tr>
@@ -76,7 +95,7 @@
 								<label for="formulaire">Email :</label>
 							</td>
 							<td>
-								<input type="email" placeholder="....@...." id="email"name="email" />
+								<input type="email" placeholder="....@...." id="email"name="email" value="<?php if(isset($email)) { echo $email; }?>"  />
 							</td>
 						</tr>
 						<tr>
@@ -84,7 +103,7 @@
 								<label for="formulaire">Numéro de telephone :</label>
 							</td>
 							<td>
-								<input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" name="numtel">
+								<input type="tel" placeholder="Votre numéro de telephone" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" name="numtel" value="<?php if(isset($numtel)) { echo $numtel; }?>" >
 							</td>
 						</tr>
 						<tr>
@@ -92,7 +111,7 @@
 								<label for="formulaire">Mot de Passe :</label>
 							</td>
 							<td>
-								<input type="password" id="mdp"name="mdp" />
+								<input type="password" placeholder="Votre mot de passe"id="mdp"name="mdp" />
 							</td>
 						</tr>
 						<tr>
@@ -100,7 +119,7 @@
 								<label for="formulaire">Confirmer le mot de passe :</label>
 							</td>
 							<td>
-								<input type="password" id="mdp2"name="mdp2" />
+								<input type="password" placeholder="Réécriver votre mdp" id="mdp2"name="mdp2" />
 							</td>
 						</tr>
 
@@ -108,7 +127,7 @@
 							<td></td> 
 							<td>
 								</br></br>
-							<input type="submit" id="Boutton_Connex" name="Boutton_Connex" value="Connexion"/>
+							<input type="submit" id="boutton_creer" name="boutton_creer" value="Créer compte"/>
 							</td>
 						</tr>
 					</table>
