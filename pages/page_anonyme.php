ddab0f879bb00c8f126d948aa4a1dfd00f586669
LucChereau <luc.chereau@etu.univ-tours.fr>
2021-01-17 18:38:34 +0100

    Update Page anonyme

--- pages/page_anonyme.php
+++ pages/page_anonyme.php
@@ -14,11 +14,27 @@ session_start();
 		$infoautre = $reqautre->fetch();
 		$infoNP = $reqNP->fetch();
 
+		$count1=0;
+		$count2=0; 
+		$count3=0; 
+		$count4=0; 
 		$reqage=$BDD->query('SELECT * FROM personne'); 
 		while($infoage=$reqage->fetch()){
 			$dateNaissance = "15-06-1995";
   			$aujourdhui = date("Y-m-d");
   			$diff = date_diff(date_create($dateNaissance), date_create($aujourdhui));
+  			if($diff->format('%y')<'25'){
+  				$count1=$count1+1; 
+  			}
+  			if($diff->format('%y')>'24' && $diff->format('%y')<'46'){
+  				$count2=$count2+1; 
+  			}
+  			if($diff->format('%y')>'45' && $diff->format('%y')<'66'){
+  				$count3=$count3+1; 
+  			}
+  			if($diff->format('%y')>'64'){
+  				$count4=$count4+1; 
+  			}
 		}
 
 
@@ -30,6 +46,7 @@ session_start();
   <title>BDD Projet</title>
   <link rel="stylesheet" href="style.css">
   <script src="package/dist/Chart.bundle.js"></script>
+
 </head>
 <body>
 	<header>
@@ -47,7 +64,9 @@ session_start();
 			</ul>
 		</header>
 	Ceci est une page anonyme 
+	<div id="container" style="width: 50%">
 	<canvas id="nbgenre"> </canvas>
+</div>
 
 <script>
 var ctx = document.getElementById('nbgenre').getContext('2d');
@@ -89,10 +108,14 @@ var nbgenre = new Chart(ctx, {
     }
 });
 </script>
+<<<<<<< Updated upstream
 </canvas>
 
+=======
+<div id="container" style="width: 50%;">
+>>>>>>> Stashed changes
 <canvas id="nbpersonne"> </canvas>
-
+</div>
 <script>
 var ctx = document.getElementById('nbpersonne').getContext('2d');
 var nbpersonne = new Chart(ctx, {
@@ -101,7 +124,7 @@ var nbpersonne = new Chart(ctx, {
        labels: ['18-24', '24-45','45-65','+65'],
         datasets: [{
         	label : ['Nombre'],
-            data: [<?php echo $infohomme['nbhomme'] ?>, <?php echo $infofemme['nbfemme'] ?>, <?php echo $infoautre['nbautre'] ?>, <?php echo $infoNP['nbnp'] ?>],
+            data: [<?php echo $count1 ?>, <?php echo $count2 ?>, <?php echo $count3 ?>, <?php echo $count4 ?>],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
