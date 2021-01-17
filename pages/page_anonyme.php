cf4bdb5e035783ceff14c4a0f260d4f66016b343
LucChereau <luc.chereau@etu.univ-tours.fr>
2021-01-17 16:37:15 +0100

    Update Page_anonyme

--- pages/page_anonyme.php
+++ pages/page_anonyme.php
@@ -28,11 +28,11 @@ session_start();
 <script>
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'pie',
+    type: 'bar',
     data: {
-       
+       labels: ['Homme', 'Femme','Autre','Non Précisé'],
         datasets: [{
-            label: '# of Votes',
+        	label : ['Nombre'],
             data: [<?php echo $infohomme['nbhomme'] ?>, <?php echo $infofemme['nbfemme'] ?>, <?php echo $infoautre['nbautre'] ?>, <?php echo $infoNP['nbnp'] ?>],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -46,11 +46,12 @@ var myChart = new Chart(ctx, {
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
+
                 
             ],
             borderWidth: 1
         }],
-         labels: ['Homme', 'Femme','Autre','Non Précisé']
+         
     },
     options: {
         scales: {
