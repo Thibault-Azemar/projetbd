5f25639400193c1e1ae7e6e795ef532392b262f7
LucChereau <luc.chereau@etu.univ-tours.fr>
2021-01-17 17:12:37 +0100

    Commit page anonyme

--- pages/page_anonyme.php
+++ pages/page_anonyme.php
@@ -13,6 +13,16 @@ session_start();
 		$infofemme = $reqfemme->fetch();
 		$infoautre = $reqautre->fetch();
 		$infoNP = $reqNP->fetch();
+
+		$reqage=$BDD->query('SELECT * FROM personne'); 
+		while($infoage=$reqage->fetch()){
+			$dateNaissance = "15-06-1995";
+  			$aujourdhui = date("Y-m-d");
+  			$diff = date_diff(date_create($dateNaissance), date_create($aujourdhui));
+		}
+
+
+
 		?>
 <html lang="fr">
 <head>
@@ -23,11 +33,11 @@ session_start();
 </head>
 <body>
 	Ceci est une page anonyme 
-	<canvas id="myChart"></canvas>
+	<canvas id="nbgenre"> </canvas>
 
 <script>
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+var ctx = document.getElementById('nbgenre').getContext('2d');
+var nbgenre = new Chart(ctx, {
     type: 'bar',
     data: {
        labels: ['Homme', 'Femme','Autre','Non Précisé'],
@@ -64,6 +74,49 @@ var myChart = new Chart(ctx, {
     }
 });
 </script>
+
+<canvas id="nbpersonne"> </canvas>
+
+<script>
+var ctx = document.getElementById('nbpersonne').getContext('2d');
+var nbpersonne = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+       labels: ['18-24', '24-45','45-65','+65'],
+        datasets: [{
+        	label : ['Nombre'],
+            data: [<?php echo $infohomme['nbhomme'] ?>, <?php echo $infofemme['nbfemme'] ?>, <?php echo $infoautre['nbautre'] ?>, <?php echo $infoNP['nbnp'] ?>],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+
+                
+            ],
+            borderWidth: 1
+        }],
+         
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
+
 
 </body>
 </html>
