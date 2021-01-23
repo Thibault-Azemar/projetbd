b557d32b13a68ab4ce244a36cfbfd3184d054225
VincentBlanco <75444092+VincentBlanco@users.noreply.github.com>
2021-01-23 16:16:48 +0100

    tableau de bord v1

--- pages/tableau_de_bord.php
+++ pages/tableau_de_bord.php
@@ -10,6 +10,7 @@
       <title>Projet BDD</title>
       <meta charset="utf-8">
 	    <link rel="stylesheet" href="style.css">
+		<script src="package/dist/Chart.bundle.js"></script>
 
    </head>
 
@@ -31,6 +32,44 @@
 			</ul>
 
 		</header>
-		
+		<div id="container" style="width: 50%">
+		<canvas id="nbgenre"> </canvas>
+		</div>
+		<script>
+		var ctx = document.getElementById('nbgenre').getContext('2d');
+		var myLineChart = new Chart(ctx, 
+		{
+			type: 'line',
+			data: 
+			{
+				labels: [	'0h00', '1h00', '2h00', '3h00', '4h00', '5h00', '6h00', '7h00', '8h00',
+							'9h00', '10h00', '11h00', '12h00', '13h00', '14h00', '15h00', '16h00', 
+							'17h00', '18h00', '19h00', '20h00', '21h00', '22h00', '23h00'
+						],
+				datasets: 
+				[{
+					label: 'Aujourd\'hui',
+					data: [12, 19, 3, 5, 2, 3],
+					borderWidth: 1
+				}]
+				
+			},
+			options: 
+			{
+				scales: 
+				{
+					yAxes: 
+					[{
+						ticks: 
+						{
+							beginAtZero: true
+						}
+					}]
+				}
+			}
+		});
+		</script>
+
+		</canvas>
 	</body>
 </html>
